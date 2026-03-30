7721a69a4db6dccaa6445b30a15b6ff3d22d80bb
miaohaonan10-boop <miaohaonan10@gmail.com>
2026-03-30 15:01:56 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/zhaoyu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zhaoyu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.green,
+
+        textTheme: GoogleFonts.aladinTextTheme(
+          ThemeData.light().textTheme,
+        ),
+      ),
+
+      home: const HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter - 조우'),
+
+        actions: const [
+          Icon(Icons.shopping_cart, color: Colors.pink,),
+          SizedBox(width: 16),
+          Icon(Icons.search, color: Colors.red),
+          SizedBox(width: 16),
+          Icon(Icons.settings, color: Colors.purple),
+          SizedBox(width: 12),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: GoogleFonts.aladin(
+            fontSize: 48,
+            fontWeight: FontWeight.w700,
+            fontStyle: FontStyle.italic,
+            color: Colors.purple,
+          ),
+        ),
+      ),
+    );
+  }
+}
